7d44743c394065317f945b31a0bbaa3b38285cc8
Kayanr <kayan.reid@gmail.com>
2020-06-19 13:05:19 -0400

    Added rest folder to project
    Added rest folder to project

---
+++ libraryms-app-rest/src/main/java/com/capstone/librarymsapprest/LibrarymsAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.librarymsapprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymsAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymsAppRestApplication.class, args);
+	}
+
+}
---
+++ libraryms-app-rest/src/test/java/com/capstone/librarymsapprest/LibrarymsAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.librarymsapprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymsAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
