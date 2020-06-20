ace6e51c4ad8267eb0cf50f4f0a9edaa4c2004eb
Kayanr <kayan.reid@gmail.com>
2020-06-20 15:19:11 -0400

    Added Toast
    added Toast which are lightweight notifications designed to mimic the push notifications that have been popularized by mobile and desktop operating systems.

--- libraryms-app-web/src/components/Book.js
+++ libraryms-app-web/src/components/Book.js
@@ -9,13 +9,14 @@ import {
   faSave,
   faUndo,
 } from "@fortawesome/free-solid-svg-icons";
+import MyToast from "./MyToast";
 
 export default class Book extends Component {
   constructor(props) {
     super(props);
-    this.state = { title: "", author: "", isbn: "", rating: "" };
 
     this.state = this.initialState;
+    this.state.show = false;
     this.bookChange = this.bookChange.bind(this);
     this.submitBook = this.submitBook.bind(this);
   }
@@ -66,86 +67,91 @@ export default class Book extends Component {
   render() {
     const { title, author, isbn, rating } = this.state;
     return (
-      <Card className={"border border-dark bg-dark text-white"}>
-        <Card.Header>
-          <FontAwesomeIcon icon={faPlusSquare} />
-          Add Book
-        </Card.Header>
-        <Form
-          onReset={this.resetBook}
-          onSubmit={this.submitBook}
-          id="bookFormId"
-        >
-          <Card.Body>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridTitle">
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  required
-                  autoComplete="off"
-                  type="test"
-                  name="title"
-                  value={title}
-                  onChange={this.bookChange}
-                  className={"bg-dark text-white"}
-                  placeholder="Enter Book Title"
-                />
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridAuthor">
-                <Form.Label>Author</Form.Label>
-                <Form.Control
-                  required
-                  autoComplete="off"
-                  type="test"
-                  name="author"
-                  value={author}
-                  onChange={this.bookChange}
-                  className={"bg-dark text-white"}
-                  placeholder="Enter Book Author"
-                />
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridIsbn">
-                <Form.Label>Isbn</Form.Label>
-                <Form.Control
-                  required
-                  autoComplete="off"
-                  type="test"
-                  name="isbn"
-                  value={isbn}
-                  onChange={this.bookChange}
-                  className={"bg-dark text-white"}
-                  placeholder="Enter Book ISBN"
-                />
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridRating">
-                <Form.Label>Rating</Form.Label>
-                <Form.Control
-                  required
-                  autoComplete="off"
-                  type="test"
-                  name="rating"
-                  value={rating}
-                  onChange={this.bookChange}
-                  className={"bg-dark text-white"}
-                  placeholder="Enter Book Rating"
-                />
-              </Form.Group>
-            </Form.Row>
-          </Card.Body>
-          <Card.Footer style={{ textAlign: "right" }}>
-            <Button size="sm" variant="success" type="submit">
-              <FontAwesomeIcon icon={faSave} />
-              Submit {""}
-            </Button>{" "}
-            <Button size="sm" variant="info" type="reset">
-              <FontAwesomeIcon icon={faUndo} />
-              Reset
-            </Button>
-          </Card.Footer>
-        </Form>
-      </Card>
+      <div>
+        <div style={{ display: this.state.show ? "block" : "none" }}>
+          <MyToast />
+        </div>
+        <Card className={"border border-dark bg-dark text-white"}>
+          <Card.Header>
+            <FontAwesomeIcon icon={faPlusSquare} />
+            Add Book
+          </Card.Header>
+          <Form
+            onReset={this.resetBook}
+            onSubmit={this.submitBook}
+            id="bookFormId"
+          >
+            <Card.Body>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridTitle">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control
+                    required
+                    autoComplete="off"
+                    type="test"
+                    name="title"
+                    value={title}
+                    onChange={this.bookChange}
+                    className={"bg-dark text-white"}
+                    placeholder="Enter Book Title"
+                  />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridAuthor">
+                  <Form.Label>Author</Form.Label>
+                  <Form.Control
+                    required
+                    autoComplete="off"
+                    type="test"
+                    name="author"
+                    value={author}
+                    onChange={this.bookChange}
+                    className={"bg-dark text-white"}
+                    placeholder="Enter Book Author"
+                  />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridIsbn">
+                  <Form.Label>Isbn</Form.Label>
+                  <Form.Control
+                    required
+                    autoComplete="off"
+                    type="test"
+                    name="isbn"
+                    value={isbn}
+                    onChange={this.bookChange}
+                    className={"bg-dark text-white"}
+                    placeholder="Enter Book ISBN"
+                  />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridRating">
+                  <Form.Label>Rating</Form.Label>
+                  <Form.Control
+                    required
+                    autoComplete="off"
+                    type="test"
+                    name="rating"
+                    value={rating}
+                    onChange={this.bookChange}
+                    className={"bg-dark text-white"}
+                    placeholder="Enter Book Rating"
+                  />
+                </Form.Group>
+              </Form.Row>
+            </Card.Body>
+            <Card.Footer style={{ textAlign: "right" }}>
+              <Button size="sm" variant="success" type="submit">
+                <FontAwesomeIcon icon={faSave} />
+                Submit {""}
+              </Button>{" "}
+              <Button size="sm" variant="info" type="reset">
+                <FontAwesomeIcon icon={faUndo} />
+                Reset
+              </Button>
+            </Card.Footer>
+          </Form>
+        </Card>
+      </div>
     );
   }
 }
